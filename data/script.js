be7c676e06389c9033b5236544f4b7f3167ff9d6
Kvz666 <55993619+Kvz666@users.noreply.github.com>
2021-10-28 23:11:15 +0200

    Destkop version finished

--- data/script.js
+++ data/script.js
@@ -1,79 +1,54 @@
-let slider = document.getElementById("mySlider");
-let discountResult = document.querySelector(".discount");
-let views = document.getElementById("number-pageviews");
-let price = document.getElementById("price");
+const range = document.getElementById('mySlider');
+const discount = document.getElementById('billing');
 
-views.innerHTML = 100 + "K";
-
-slider.oninput = function () {
-  switch (parseInt(this.value)) {
-    case 0:
-      views.innerHTML = 10 + 'K';
-      break;
-    case 1:
-      views.innerHTML = 100 + "K";
-      break;
-    case 2:
-      views.innerHTML = 500 + 'K';
-      break;
-
-      case 3:
-      views.innerHTML = 1 + 'M';
-      break;
-
-  }
+const rangePricing = {
+    1: {
+        views: '10K',
+        price: 8
+    },
+    2: {
+        views: '50K',
+        price: 12
+    },
+    3: {
+        views: '100K',
+        price: 16
+    },
+    4: {
+        views: '500K',
+        price: 24
+    },
+    5: {
+        views: '1M',
+        price: 36
+    }
 };
 
-slider.addEventListener('mousemove', function(){
-    let value = slider.value;
-    let colorSlide = `linear-gradient(to right, var(--full-Slider-Bar))` + value + `%, var(--full-Slider-Bar)` + x + `%, var(--empty-slider-bar)` + value `%,var(--empty-slider-bar)`;
-    slider.style.backgroun = colorSlide;
+range.oninput = function(){
+    let trackValue = (this.value-this.min)/(this.max-this.min)*100;
+    this.style.background = 
+    'linear-gradient(to right, var(--slider-bar) 0%, var(--slider-bar) ' + trackValue 
+    + '%, var(--empty-slider-bar) ' + trackValue + '%, var(--empty-slider-bar) 100%)'
+  };
+
+
+showCurrentPrice(+range.value);
+
+function showCurrentPrice(value) {
+    const currentPrice = rangePricing[value];
+
+    const price = discount.checked ? currentPrice.price - currentPrice.price * .25 : currentPrice.price;
+    document.getElementById('number-pageviews').innerHTML = currentPrice.views;
+    document.getElementById('price-month').innerHTML = `$${price.toFixed(2)}`;
+}
+
+range.addEventListener('change', (event) => {
+    showCurrentPrice(+event.target.value);
+})
+
+discount.addEventListener('change', () => {
+    showCurrentPrice(+range.value);
 })
 
 
-
-// window.onload = function() {
-//     sliderChange();
-// };
-
-// discountSwitch.onclick = function () {
-//     this.classList.toggle('switch-active')
-//     if (this.classList.contains('switch-active')) {
-//         discount = true
-//     }else {
-//         discount = false
-//     }
-//     sliderChange();
-// }
-
-// slider.oninput = function () {
-//     sliderChange();
-// }
-
-// const sliderChange = ()=> {
-//     let percent = (slider.value-slider.min)/(slider.max-slider.min)*100;
-//     let value = slider.value;
-//     slider.style.background = `linear-gradient(to right, var(--sliderBg) 0%, var(--sliderBg) ${percent}%, var(--sliderBgAfterThumb) ${percent}%, var(--sliderBgAfterThumb) 100%)`;
-//     switch (value) {
-//         case '0':
-//             views.innerText = '10k';
-//             price.innerText =`$${(8 * (discount ? 0.75 : 1)).toFixed(2)}`
-//             break;
-//         case '1':
-//             views.innerText = '50k';
-//             price.innerText =`$${(12 * (discount ? 0.75 : 1)).toFixed(2)}`
-//             break;
-//         case '2':
-//             views.innerText = '100k';
-//             price.innerText = `$${(16 * (discount ? 0.75 : 1)).toFixed(2)}`
-//             break;
-//         case '3':
-//             views.innerText = '500k';
-//             price.innerText = `$${(24 * (discount ? 0.75 : 1)).toFixed(2)}`
-//             break;
-//         case '4':
-//             views.innerText = '1m';
-//             price.innerText = `$${(36 * (discount ? 0.75 : 1)).toFixed(2)}`
-//             break;
-//     }
-// }
+  
